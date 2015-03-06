582bb67cfc505ddd56b041a9cbe337a0a48216fd
sathishkumar <sathish4fri@gmail.com>
2015-03-06 11:17:43 +0530

    Update and rename forms/administrator to forms/administrator/settings.php

---
+++ forms/administrator/settings.php
@@ -0,0 +1,5 @@
+<label>Contact</label>
+<label>Enter the email that you want to receive feedback</label>
+<input type="text" name="email" value="<?php echo $params['settings']->email;?>"/>
+<!--<textarea id="gmap" name="gmap" placeholder="Enter Your google map iframe" ><?php echo $params['settings']->gmap;?></textarea>-->
+<input type="submit" class="ossn-admin-button button-dark-blue" value="<?php echo ossn_print('save'); ?>"/>
