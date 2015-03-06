7b11d28af6ce9b29fecb12e7a5113c516078bc64
sathishkumar <sathish4fri@gmail.com>
2015-03-06 11:18:34 +0530

    Create ossn.en.php

---
+++ locale/ossn.en.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ *    OpenSource-SocialNetwork
+ *
+ * @package   (webbehinds.com).ossn
+ * @author    OSSN Core Team | Sathish Kumar <info@opensource-socialnetwork.com>
+ * @copyright 2014 webbehinds
+ * @license   General Public Licence http://opensource-socialnetwork.com/licence
+ * @link      http://www.opensource-socialnetwork.com/licence
+ */
+$en = array(
+    'contact' => 'Contact',
+    'contact:body' => 'Name 	: %s
+
+Email 	: %s		
+
+Message  : %s ',
+);
+ossn_register_languages('en', $en); 
