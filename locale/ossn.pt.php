a4ef18a00d06bc2c583f6d16cc1ecf51b074e288
Rafael Amorim <3620428+rafaelmamorim@users.noreply.github.com>
2023-03-23 18:24:24 -0300

    Added PT translated file

--- locale/ossn.pt.php
+++ locale/ossn.pt.php
@@ -8,34 +8,31 @@
  * @license   Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
  * @link      https://www.opensource-socialnetwork.org/
  */
-$en = array(
+$pt = array(
 	// site frontend
-    'contact:form:title' => 'Contact Us',
-	'contact:form:description' => 'Please fill out all fields',
-	'contact:form:name' => 'Your Name',
-	'contact:form:email' => 'Your Email',
-	'contact:form:message' => 'Message',
-	'contact:form:button:send' => 'Submit',
+    'contact:form:title' => 'Contato',
+	'contact:form:description' => 'Todos os campos são obrigatórios',
+	'contact:form:name' => 'Seu nome',
+	'contact:form:email' => 'Seu e-mail',
+	'contact:form:message' => 'Mensagem',
+	'contact:form:button:send' => 'Enviar',
 	
-	'contact:form:message:sent' => 'Your Message was sent!',
-	'contact:form:message:incomplete' => 'Please fill out all fields',
-	'contact:form:message:not:sent' => 'Your message could not be sent due to a technical problem. Please try again later.',
+	'contact:form:message:sent' => 'Sua mensagem foi enviada!',
+	'contact:form:message:incomplete' => 'Todos os campos são obrigatórios',
+	'contact:form:message:not:sent' => 'Devido a problemas técnicos, sua mensagem não foi enviada. Tente mais tarde.',
 
 	// admin backend
 	'contact' => 'Contact', // Configure menu entry - don't translate
-	'contact:admin:form:label:contactform:receiver' => 'Email address to which the contact form should be sent',
-	'contact:admin:form:label:contactform:top:html' => 'Any text or HTML that should appear above the contact form, e.g. an IFRAME with a road map',
-	'contact:admin:form:button:save' => 'Save',
-	'contact:admin:settings:email:invalid' => 'Please enter a valid email address',
+	'contact:admin:form:label:contactform:receiver' => 'Endereço de e-mail para o qual a mensagem deve ser enviada',
+	'contact:admin:form:label:contactform:top:html' => 'Qualquer texto ou HTML que deve aparecer acima do formulário de contato, por exemplo um IFRAME com uma página ou mapa',
+	'contact:admin:form:button:save' => 'Salvar',
+	'contact:admin:settings:email:invalid' => 'Informe um endereço de e-mail válido',
 
 	// mailer
-	'contact:mail:subject' => 'Contact request',
+	'contact:mail:subject' => 'Formulário de contato',
 	'contact:mail:body' => '
-You received a message from
-
-Name : %s
-Email: %s	
-
+Você recebeu uma mensagem de %s (%s): 	
 %s ',
 );
-ossn_register_languages('pt', $en); 
+
+ossn_register_languages('pt', $pt); 
