27b50c21e666b573a2f507c44d2818323f467788
Rafael Amorim <3620428+rafaelmamorim@users.noreply.github.com>
2023-03-23 18:32:22 -0300

    Added reply to and improved messages to users

--- actions/contactmail.php
+++ actions/contactmail.php
@@ -8,7 +8,7 @@
  * @license   General Public Licence http://opensource-socialnetwork.com/licence
  * @link      http://www.opensource-socialnetwork.com/licence
  */
-$subject= "Contact form Page";
+$subject = ossn_print('contact:mail:subject');
 $message = input('message');
 $message = html_entity_decode($message, ENT_QUOTES, "UTF-8");
 $message = strip_tags($message);
@@ -16,30 +16,23 @@ $message = ossn_restore_new_lines($message);
 $name = input('name');
 $cemail = input('email');
 
-$body = ossn_print('contact:body', array(
-                        $name,
-                        $cemail,
-                        $message
-                    ));
+$body = ossn_print('contact:body', array($name, $cemail, $message));
+
 $settings = new OssnComponents;
 $settings = $settings->getComSettings('contact');
 $email = $settings->email;
 
-if(!empty($message) && !empty($cemail)){
-$mail = new OssnMail;
-if($mail->NotifiyUser($email, $subject, $body)){
-ossn_trigger_message('Mail Sent');
-redirect(REF);
-}
-else
-{
- ossn_trigger_message('mail not sent', 'error');
-redirect(REF);
+if (!empty($message) && !empty($cemail)) {
+    $mail = new Contact;
+    if ($mail->NotifiyUserWithReplyTo($email, $subject . "  - ID: " . time(), $body, $cemail, $name)) { //add id = timestamp to prevent group by subject 
+        ossn_trigger_message(ossn_print('contact:form:message:sent'));
+        redirect(REF);
+    } else {
+        ossn_trigger_message(ossn_print('contact:form:message:not:sent'), 'error');
+        redirect(REF);
 
+    }
+} else {
+    ossn_trigger_message(ossn_print('contact:form:message:incomplete'), 'error');
+    redirect(REF);
 }
-}else
-{
-ossn_trigger_message('Please fill all the details', 'error');
-redirect(REF);
-}
-
